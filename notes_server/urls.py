89dbee800dfabdef6d0dfa96dd2f2acdc9060b2f
khushi <khushi112200311@gmail.com>
2026-02-26 18:16:15 +0530

    Removed temp admin route

--- notes_server/urls.py
+++ notes_server/urls.py
@@ -1,9 +1,9 @@
 
 from django.conf import settings
-from simply_notes.views import create_admin
+
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls)
