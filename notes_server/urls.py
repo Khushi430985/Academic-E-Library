1b62d51e403bac0bbaa15ea3c6bae35ad1a2a02d
khushi <khushi112200311@gmail.com>
2026-02-26 17:38:34 +0530

    Fix create admin route

--- notes_server/urls.py
+++ notes_server/urls.py
@@ -1,10 +1,11 @@
 
 from django.conf import settings
+from simply_notes.views import create_admin
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('simply_notes.urls')),
+    path('create-admin/', create_admin),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
