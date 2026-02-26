e4738bf7f650c10ab507ddd0013c1ff262ff92fd
khushi <khushi112200311@gmail.com>
2026-02-26 18:08:49 +0530

    Fix create admin route

--- notes_server/urls.py
+++ notes_server/urls.py
@@ -6,6 +6,5 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('create-admin/', create_admin),
+    path('admin/', admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- simply_notes/views.py
+++ simply_notes/views.py
@@ -387,15 +387,3 @@ def password_reset_request(request):
     return render(request, "password_reset_form.html")
 
 
-from django.contrib.auth import get_user_model
-from django.http import HttpResponse
-
-def create_admin(request):
-    User = get_user_model()
-    if not User.objects.filter(email="admin@gmail.com").exists():
-        User.objects.create_superuser(
-            email="admin@gmail.com",
-            password="admin123"
-        )
-        return HttpResponse("Admin created successfully")
-    return HttpResponse("Admin already exists")
